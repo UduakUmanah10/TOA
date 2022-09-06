2015dc5d9a9008d52cc815704122a1fd801ee1b6
uduak <uduakumanah10@gmail.com>
2022-09-06 09:27:34 +0100

    completing primary Button

--- app/src/main/java/com/uduak/toa/MainActivity.kt
+++ app/src/main/java/com/uduak/toa/MainActivity.kt
@@ -3,6 +3,7 @@ package com.uduak.toa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.uduak.toa.ui.components.PrimaryButton
 import com.uduak.toa.ui.theme.TOATheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +22,12 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background)
                 {
-                    Greeting("Android")
+                    Column{Greeting("Android")
+                    PrimaryButton(
+                        text= "primary Button",
+                        onClick = {}
+                    )
+                    }
                 }
 
             }
--- app/src/main/java/com/uduak/toa/ui/components/PrimaryButton.kt
+++ app/src/main/java/com/uduak/toa/ui/components/PrimaryButton.kt
@@ -1,7 +1,9 @@
 package com.uduak.toa.ui.components
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.MaterialTheme
@@ -9,9 +11,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.uduak.toa.R
 import com.uduak.toa.ui.theme.TOATheme
 
 
@@ -29,12 +35,14 @@ fun PrimaryButton(
 
     Button(
         onClick = onClick,
+        shape = RoundedCornerShape(50) ,
         modifier = modifier
-            .height(48.dp),
+            .height(dimensionResource(id = R.dimen.button_height))
+            .fillMaxWidth(),
         colors =buttonColors
 
     ) {
-        Text(text="Button")
+        Text(text= text.toUpperCase(Locale.current))
     }
 
 }
@@ -56,7 +64,7 @@ fun PrimaryButton(
 private  fun PrimaryButtonPreview(){
     TOATheme{
         PrimaryButton(
-            text= "primary Button",
+            text = "primary Button",
             onClick = {}
         )
 
